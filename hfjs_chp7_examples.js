39a2b57b0b132f238e8765d5a85cb7ad0f2adb2c
culpepperd <culpepperd2490@gmail.com>
2023-06-20 13:17:10 -0400

    In The Laboratory typeof operator examples

--- hfjs_chp7_examples.js
+++ hfjs_chp7_examples.js
@@ -12,6 +12,31 @@ if (customer.phoneNumber == undefined) {
 	console.log("customer.phoneNumber is: " + customer.phoneNumber);
 }
 
+// pg. 269 - "In The Laboratory" - investigating using the typeof operator
+var subject = "Just a string";
+var probe = typeof subject;
+console.log("The subject variable is of type: " + probe);
+
+var test1 = "abcdef";
+var test2 = 123;
+var test3 = true;
+var test4 = {};
+var test5 = [];
+var test6;
+var test7 = { "abcdef": 123 };
+var test8 = ["abcdef", 123];
+function test9() { return "abcdef" };
+
+console.log(typeof test1);
+console.log(typeof test2);
+console.log(typeof test3);
+console.log(typeof test4);
+console.log(typeof test5);
+console.log(typeof test6);
+console.log(typeof test7);
+console.log(typeof test8);
+console.log(typeof test9);
+
 
 /********************************************************************************************/
 
